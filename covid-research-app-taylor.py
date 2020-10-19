3c4c7a88b5cf2f89ce012b787b083e8c4172f371
Matt Martin <matt.martin@hey.com>
2020-10-18 21:17:11 -0500

    refactored chart code to functions

--- covid-research-app-taylor.py
+++ covid-research-app-taylor.py
@@ -1,4 +1,4 @@
-#import packges to be used 
+### IMPORT PACKAGES TO BE USED ### 
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -10,25 +10,53 @@ alt.data_transformers.disable_max_rows()
 #setting variables which will contain geojson for county boundaries/mapping and COVID-19 case data from Texas Department of Health and Human Services
 counties = alt.topo_feature('https://raw.githubusercontent.com/deldersveld/topojson/master/countries/us-states/TX-48-texas-counties.json','cb_2015_texas_county_20m')
 
-#import weekly case data
+
+
+
+### IMPORT DATA ###
+
+## import weekly case data
 url_weekly_cases = 'https://raw.githubusercontent.com/msquaredsa-ds-dev/covid-19-streamlit/master/cases-weekly-processed.csv'
 weekly_cases = pd.read_csv(url_weekly_cases,parse_dates=['date'])
 
-#import cumulative case data
+## import cumulative case data
 url_cumulative_cases = 'https://raw.githubusercontent.com/msquaredsa-ds-dev/covid-19-streamlit/master/cases-cumulative-processed.csv'
 cumulative_cases = pd.read_csv(url_cumulative_cases,parse_dates=['date'])
 
-#import weekly death data
+## import weekly death data
 url_weekly_deaths = 'https://raw.githubusercontent.com/msquaredsa-ds-dev/covid-19-streamlit/master/deaths-weekly-processed.csv'
 weekly_deaths = pd.read_csv(url_weekly_deaths,parse_dates=['date'])
 
-#import cumulative death data
+## import cumulative death data
 url_cumulative_deaths = 'https://raw.githubusercontent.com/msquaredsa-ds-dev/covid-19-streamlit/master/deaths-cumulative-processed.csv'
 cumulative_deaths = pd.read_csv(url_cumulative_deaths,parse_dates=['date'])
 
+## import daily case and death data from San Antonio Metropolitan Health
+url_daily_cases_deaths_sa = 'https://raw.githubusercontent.com/msquaredsa-ds-dev/covid-19-streamlit/master/SAMHD_Daily_Surveillance_Data_Public.csv'
+daily_sa_df = pd.read_csv(url_daily_cases_deaths_sa,parse_dates=['reporting_date'])
+sa_cases_deaths_df = daily_sa_df.resample('W-Mon',label='right',closed='right',on='reporting_date').sum().reset_index()[['reporting_date','total_case_daily_change','deaths_daily_change']]
+
+
+
+
+### COUNTY LISTS ###
+bexar_county_list = ['Bexar', 'Medina', 'Bandera','Kendall','Comal','Guadalupe','Wilson','Atascosa']
+travis_county_list = ['Travis', 'Hays', 'Blanco','Burnet','Williamson','Lee','Bastrop','Caldwell']
+harris_county_list = ['Waller', 'Montgomery', 'Harris','Liberty','Chambers','Galveston','Brazoria','Fort Bend']
+dallas_county_list = ['Dallas', 'Tarrant', 'Denton','Collin','Rockwall','Kaufman','Ellis','Johnson']
+
+
+
+
+### CREATE WIDGETS ###
+
+## Create widget for toggling between Incidence and Mortality
 metric = st.sidebar.radio('Measure:',('Incidence','Mortality'))
 
-#Create date slider widget
+## Create widget for toggling between metros
+major_metro = st.sidebar.radio('Major Metro:',('San Antonio','Austin','Houston','Dallas'))
+
+## Create date slider widget
 if metric == 'Incidence':
     min_date = weekly_cases['date'].min()
     max_date = weekly_cases['date'].max()
@@ -42,79 +70,74 @@ elif metric == 'Mortality':
     date_slider = st.sidebar.slider('Date',min_value=min_date.date(),max_value=max_date.date(),step=timedelta(days=7))
     date_value = date_slider.strftime('%B %d, %Y')
 
-selection = alt.selection_single(on='mouseover', empty='none')
-
-#SAN ANTONIO
-st.title('Bexar County and Surrounding Counties')
-st.subheader('(Based on data from the State Dept. of Health and Human Services)')
-st.write('')
-st.write('')
-
-#create chloropleth map for San Antonio and surronding counties - Incidence
-incidence_sa_chart = alt.Chart(counties).mark_geoshape(stroke='grey').encode(
-    color=alt.condition(selection, alt.value('black'), 'cases-per-100K:Q'),
-    tooltip=['[properties][NAME]:N','cases:Q','population:Q','cases-per-100K:Q']
-).transform_lookup(
-    lookup='[properties][NAME]',
-    from_=alt.LookupData(cumulative_cases[cumulative_cases['date'] == date_value],'county',['cases','population','cases-per-100K'])
-).properties(
-    width=800,
-    height=600
-).add_selection(
-    selection
-).transform_filter(
-    alt.FieldOneOfPredicate(field='[properties][NAME]', oneOf=['Bexar', 'Medina', 'Bandera','Kendall','Comal','Guadalupe','Wilson','Atascosa'])
-)
-
-#create chloropleth map for San Antonio and surronding counties - Mortality
-mortality_sa_chart = alt.Chart(counties).mark_geoshape(stroke='grey').encode(
-    color=alt.condition(selection, alt.value('black'), 'deaths-per-100K:Q'),
-    tooltip=['[properties][NAME]:N','deaths:Q','population:Q','deaths-per-100K:Q']
-).transform_lookup(
-    lookup='[properties][NAME]',
-    from_=alt.LookupData(cumulative_deaths[cumulative_deaths['date'] == date_value],'county',['deaths','population','deaths-per-100K'])
-).properties(
-    width=800,
-    height=600
-).add_selection(
-    selection
-).transform_filter(
-    alt.FieldOneOfPredicate(field='[properties][NAME]', oneOf=['Bexar', 'Medina', 'Bandera','Kendall','Comal','Guadalupe','Wilson','Atascosa'])
-)
-
-if metric == 'Incidence':
-    st.write('## Cumulative Cases per 100K People Over Time')
-    st.write('')
-    st.write('')
-    st.altair_chart(incidence_sa_chart,use_container_width=False)
-elif metric == 'Mortality':
-    st.write('## Cumulative Deaths per 100K People Over Time')
-    st.write('')
-    st.write('')
-    st.altair_chart(mortality_sa_chart,use_container_width=False)
-
-st.write('')
-st.write('')
-st.write('')
-st.write('')
 
 
 
-if metric == 'Incidence':
-    st.write('## Weekly Incidence per 100K')
-    st.write('')
-    st.write('')
+### FUNCTIONS ###
+
+## Function to create a chloropleth map using cumulative data
+def cumulative_per_100k_over_time(metric, data, county_list):
+
+    if metric == 'Incidence':
+        st.write('## Cumulative Cases per 100K People Over Time')
+        st.write('')
+        st.write('')
+
+        core_metric = 'cases'
+    elif metric == 'Mortality':
+        st.write('## Cumulative Deaths per 100K People Over Time')
+        st.write('')
+        st.write('')
+
+        core_metric = 'deaths'
+
+
+    selection = alt.selection_single(on='mouseover', empty='none')
+
+    chloropleth_map = alt.Chart(counties).mark_geoshape(stroke='grey').encode(
+        color=alt.condition(selection, alt.value('black'), core_metric+'-per-100K:Q'),
+        tooltip=['[properties][NAME]:N',core_metric+':Q','population:Q',core_metric+'-per-100K:Q']
+    ).transform_lookup(
+        lookup='[properties][NAME]',
+        from_=alt.LookupData(data[data['date'] == date_value],'county',[core_metric,'population',core_metric+'-per-100K'])
+    ).properties(
+        width=800,
+        height=600
+    ).add_selection(
+        selection
+    ).transform_filter(
+        alt.FieldOneOfPredicate(field='[properties][NAME]', oneOf=county_list)
+    )
+
+    render = st.altair_chart(chloropleth_map,use_container_width=False)
+    return render
+
+
+## Function to create a line chart showing non-cumulative, weekly incidence or mortality data
+def weekly_line_chart(metric, data, county_list):
+
+    if metric == 'Incidence':
+        st.write('## Weekly Incidence per 100K')
+        st.write('')
+        st.write('')
+
+        core_metric = 'cases'
+    elif metric == 'Mortality':
+        st.write('## Weekly Mortality per 100K')
+        st.write('')
+        st.write('')
+
+        core_metric = 'deaths'
 
     #create county multi-select
-    county_selection = st.multiselect('Which counties do you want to study?',['Bexar', 'Medina', 'Bandera', 'Kendall','Comal','Guadalupe','Wilson','Atascosa'],['Bexar', 'Medina', 'Bandera', 'Kendall','Comal','Guadalupe','Wilson','Atascosa'])
+    county_selection = st.multiselect('Which counties do you want to study?', county_list, county_list)
     st.write('')
 
-    #create line chart for San Antonio and surrounding counties - Incidence
-    incidence_sa_line = alt.Chart(weekly_cases).mark_line().encode(
+    line_chart = alt.Chart(data).mark_line().encode(
         x=alt.X('date',axis=alt.Axis(title='Week Starting')),
-        y=alt.Y('cases-per-100K:Q',axis=alt.Axis(title='Cases per 100K')),
+        y=alt.Y(core_metric+'-per-100K:Q',axis=alt.Axis(title=core_metric[0].upper()+core_metric[1:]+' per 100K')),
         color='county',
-        tooltip=['date','county','cases','population','cases-per-100K']
+        tooltip=['date','county',core_metric,'population',core_metric+'-per-100K']
     ).transform_filter(
         alt.FieldOneOfPredicate(field='county', oneOf=county_selection)
     ).properties(
@@ -122,68 +145,207 @@ if metric == 'Incidence':
         height=600
     )
 
-    #render chart
-    st.altair_chart(incidence_sa_line,use_container_width=False)
+    render = st.altair_chart(line_chart,use_container_width=False)
+    return render
 
-elif metric == 'Mortality':
-    st.write('## Weekly Mortality per 100K')
+
+
+
+### CREATE PAGE FOR SAN ANTONIO ###
+if major_metro == 'San Antonio':
+
+
+    ## Create titles ##
+    st.title('Bexar County and Surrounding Counties')
+    st.subheader('(Based on data from the State Dept. of Health and Human Services)')
     st.write('')
     st.write('')
 
-    #create county multi-select
-    county_selection = st.multiselect('Which counties do you want to study?',['Bexar', 'Medina', 'Bandera', 'Kendall','Comal','Guadalupe','Wilson','Atascosa'],['Bexar', 'Medina', 'Bandera', 'Kendall','Comal','Guadalupe','Wilson','Atascosa'])
+
+    ## Create chloropleth map for San Antonio and surronding counties and render based on radio button toggle for Incidence or Mortality ##
+    # Incidence
+    if metric == 'Incidence':
+        cumulative_per_100k_over_time(metric, cumulative_cases, bexar_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        cumulative_per_100k_over_time(metric, cumulative_deaths, bexar_county_list)
+
+
+
+    ### SPACING ###
+    st.write('')
+    st.write('')
+    st.write('')
     st.write('')
 
-    #create line chart for San Antonio and surrounding counties - Mortality
-    mortality_sa_line = alt.Chart(weekly_deaths).mark_line().encode(
-        x=alt.X('date',axis=alt.Axis(title='Week Starting')),
-        y=alt.Y('deaths-per-100K:Q',axis=alt.Axis(title='Deaths per 100K')),
-        color='county',
-        tooltip=['date','county','deaths','population','deaths-per-100K']
-    ).transform_filter(
-        alt.FieldOneOfPredicate(field='county', oneOf=county_selection)
-    ).properties(
-        width=900,
-        height=600
+
+    ## Create and render line chart displaying the metric (chosen based on the Incidence/Mortality toggle) per 100K on a weekly basis for Bexar and surrounding counties
+    # Incidence
+    if metric == 'Incidence':
+        weekly_line_chart(metric, weekly_cases, bexar_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        weekly_line_chart(metric, weekly_deaths, bexar_county_list)
+        
+
+
+
+    ### SPACING AND TITLES ###    
+    st.write('')
+    st.write('')
+    st.title('San Antonio')
+    st.subheader('(Based on data from San Antonio Metropolitan Health)')
+    st.write('')
+    st.write('')
+    st.write('## Weekly Incidence and Mortality - San Antonio')
+    st.write('')
+    st.write('')
+
+
+    ### CREATE DUAL AXIS CHART FOR INCIDENCE AND MORTALITY ON SA METRO HEALTH DATA
+    base = alt.Chart(sa_cases_deaths_df).encode(
+        alt.X('reporting_date',axis=alt.Axis(title='Reporting Date'))
     )
 
-    #render chart
-    st.altair_chart(mortality_sa_line,use_container_width=False)
+    sa_deaths_line = base.mark_line(color='#FF2D00').encode(
+        y=alt.Y('deaths_daily_change', axis=alt.Axis(title='Weekly Deaths',titleColor='#FF2D00'))
+    ).properties(
+        height=600,
+        width=900
+    )
 
-st.write('')
-st.write('')
-st.title('San Antonio')
-st.subheader('(Based on data from San Antonio Metropolitan Health)')
-st.write('')
-st.write('')
-st.write('## Weekly Incidence and Mortality - San Antonio')
-st.write('')
-st.write('')
+    sa_cases_line = base.mark_line(color='#000000').encode(
+        y=alt.Y('total_case_daily_change', axis=alt.Axis(title='Weekly Cases',titleColor='#000000'))
+    ).properties(
+        height=600,
+        width=900
+    )
+
+    cases_deaths_sa_layer_chart = alt.layer(sa_cases_line,sa_deaths_line).resolve_scale(y='independent').encode(tooltip=['reporting_date','total_case_daily_change','deaths_daily_change'])
+
+    st.altair_chart(cases_deaths_sa_layer_chart,use_container_width=False)
 
 
-#create
-url_daily_cases_deaths_sa = 'https://raw.githubusercontent.com/msquaredsa-ds-dev/covid-19-streamlit/master/SAMHD_Daily_Surveillance_Data_Public.csv'
-daily_sa_df = pd.read_csv(url_daily_cases_deaths_sa,parse_dates=['reporting_date'])
-sa_cases_deaths_df = daily_sa_df.resample('W-Mon',label='right',closed='right',on='reporting_date').sum().reset_index()[['reporting_date','total_case_daily_change','deaths_daily_change']]
 
-base = alt.Chart(sa_cases_deaths_df).encode(
-    alt.X('reporting_date',axis=alt.Axis(title='Reporting Date'))
-)
 
-sa_deaths_line = base.mark_line(color='#FF2D00').encode(
-    y=alt.Y('deaths_daily_change', axis=alt.Axis(title='Weekly Deaths',titleColor='#FF2D00'))
-).properties(
-    height=600,
-    width=900
-)
+### CREATE PAGE FOR AUSTIN ###
+elif major_metro == 'Austin':
 
-sa_cases_line = base.mark_line(color='#000000').encode(
-    y=alt.Y('total_case_daily_change', axis=alt.Axis(title='Weekly Cases',titleColor='#000000'))
-).properties(
-    height=600,
-    width=900
-)
 
-cases_deaths_sa_layer_chart = alt.layer(sa_cases_line,sa_deaths_line).resolve_scale(y='independent').encode(tooltip=['reporting_date','total_case_daily_change','deaths_daily_change'])
+    ## Create titles ##
+    st.title('Travis County and Surrounding Counties')
+    st.subheader('(Based on data from the State Dept. of Health and Human Services)')
+    st.write('')
+    st.write('')
 
-st.altair_chart(cases_deaths_sa_layer_chart,use_container_width=False)
+
+    ## Create chloropleth map for Travis and surronding counties and render based on radio button toggle for Incidence or Mortality ##
+    # Incidence
+    if metric == 'Incidence':
+        cumulative_per_100k_over_time(metric, cumulative_cases, travis_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        cumulative_per_100k_over_time(metric, cumulative_deaths, travis_county_list)
+
+
+
+    ### SPACING ###
+    st.write('')
+    st.write('')
+    st.write('')
+    st.write('')
+
+
+    ## Create and render line chart displaying the metric (chosen based on the Incidence/Mortality toggle) per 100K on a weekly basis for Travis and surrounding counties
+    # Incidence
+    if metric == 'Incidence':
+        weekly_line_chart(metric, weekly_cases, travis_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        weekly_line_chart(metric, weekly_deaths, travis_county_list)
+
+    
+
+
+### CREATE PAGE FOR HOUSTON ###
+elif major_metro == 'Houston':
+
+
+    ## Create titles ##
+    st.title('Harris County and Surrounding Counties')
+    st.subheader('(Based on data from the State Dept. of Health and Human Services)')
+    st.write('')
+    st.write('')
+
+
+    ## Create chloropleth map for Harris and surronding counties and render based on radio button toggle for Incidence or Mortality ##
+    # Incidence
+    if metric == 'Incidence':
+        cumulative_per_100k_over_time(metric, cumulative_cases, harris_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        cumulative_per_100k_over_time(metric, cumulative_deaths, harris_county_list)
+
+
+
+    ### SPACING ###
+    st.write('')
+    st.write('')
+    st.write('')
+    st.write('')
+
+
+    ## Create and render line chart displaying the metric (chosen based on the Incidence/Mortality toggle) per 100K on a weekly basis for Harris and surrounding counties
+    # Incidence
+    if metric == 'Incidence':
+        weekly_line_chart(metric, weekly_cases, harris_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        weekly_line_chart(metric, weekly_deaths, harris_county_list)
+
+
+
+
+### CREATE PAGE FOR DALLAS ###
+elif major_metro == 'Dallas':
+
+
+    ## Create titles ##
+    st.title('Dallas County and Surrounding Counties')
+    st.subheader('(Based on data from the State Dept. of Health and Human Services)')
+    st.write('')
+    st.write('')
+
+
+    ## Create chloropleth map for Dallas and surronding counties and render based on radio button toggle for Incidence or Mortality ##
+    # Incidence
+    if metric == 'Incidence':
+        cumulative_per_100k_over_time(metric, cumulative_cases, dallas_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        cumulative_per_100k_over_time(metric, cumulative_deaths, dallas_county_list)
+
+
+
+    ### SPACING ###
+    st.write('')
+    st.write('')
+    st.write('')
+    st.write('')
+
+
+    ## Create and render line chart displaying the metric (chosen based on the Incidence/Mortality toggle) per 100K on a weekly basis for Dallas and surrounding counties
+    # Incidence
+    if metric == 'Incidence':
+        weekly_line_chart(metric, weekly_cases, dallas_county_list)
+
+    # Mortality
+    elif metric == 'Mortality':
+        weekly_line_chart(metric, weekly_deaths, dallas_county_list)
